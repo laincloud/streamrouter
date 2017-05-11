781f9b5d63b2494989f398f4c4a02a6c6c1fd278
Jian Bai (Eric Pai) <ericpai94@hotmail.com>
2017-05-11 10:48:22 +0800

    Fix bugs of forking master processes

--- nginx_install.sh
+++ nginx_install.sh
@@ -7,8 +7,10 @@ cd /tmp
 NGINX_VERSION=1.11.4
 curl -s -o nginx-$NGINX_VERSION.tar.gz http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
 tar -zxf nginx-$NGINX_VERSION.tar.gz
-git clone https://github.com/lusis/ngx_stream_upstream_check_module.git
-cd nginx-$NGINX_VERSION
+git clone https://github.com/cybercom-finland/ngx_stream_upstream_check_module.git
+cd /tmp/ngx_stream_upstream_check_module
+git checkout fixes
+cd /tmp/nginx-$NGINX_VERSION
 patch -p0 < /tmp/ngx_stream_upstream_check_module/patch-1.11.x.patch
 
 ./configure --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --sbin-path=/usr/local/sbin/nginx --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-stream --add-module=/tmp/ngx_stream_upstream_check_module
